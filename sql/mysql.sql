4630a38f2082956371b25f98bb172b32e121a99b
Tad <tad0616@gmail.com>
2013-09-06 09:22:47 +0800

    add sql

---
+++ sql/mysql.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tad_rss (
+  `rss_sn` smallint(5) unsigned NOT NULL  auto_increment,
+  `title` varchar(255) NOT NULL default '' ,
+  `url` varchar(255) NOT NULL  default '' ,
+  `enable` enum('1','0') NOT NULL default '1' ,
+  PRIMARY KEY  (`rss_sn`)
+);
+
+
