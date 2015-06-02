7adfa29f535e37f4fd3db11f91b2abd6cebd9511
mambax7 <mambax7@gmail.com>
2015-06-02 07:02:45 -0400

    English Update

--- language/english/blocks.php
+++ language/english/blocks.php
@@ -5,6 +5,5 @@
 // $Id:$
 // ------------------------------------------------------------------------- //
 
-define("_MB_TADRSS_TAD_RSS_SHOW_EDIT_BITEM0","Select RSS to list");
-define("_MB_TADRSS_TAD_RSS_SHOW_EDIT_BITEM1","News number");
-?>
+define('_MB_TADRSS_TAD_RSS_SHOW_EDIT_BITEM0', 'Select RSS to list');
+define('_MB_TADRSS_TAD_RSS_SHOW_EDIT_BITEM1', 'News number');
