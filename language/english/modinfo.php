ccea6a35603b2845b8cdaa353f44610d9bb1eaf8
mambax7 <mambax7@gmail.com>
2015-06-02 06:58:56 -0400

    English Update

--- language/english/modinfo.php
+++ language/english/modinfo.php
@@ -4,15 +4,21 @@
 // 製作日期：2009-10-20
 // $Id:$
 // ------------------------------------------------------------------------- //
-include_once XOOPS_ROOT_PATH."/modules/tadtools/language/{$xoopsConfig['language']}/modinfo_common.php";
+include_once XOOPS_ROOT_PATH . "/modules/tadtools/language/{$xoopsConfig['language']}/modinfo_common.php";
 
-define("_MI_TADRSS_NAME","RSS News");
-define("_MI_TADRSS_AUTHOR","Tad (tad0616@gmail.com)");
-define("_MI_TADRSS_CREDITS","tad");
-define("_MI_TADRSS_DESC","Adding RSS/Atom syndicated content inside your XOOPS Site.");
-define("_MI_TADRSS_ADMENU1", "Admin");
-define("_MI_TADRSS_BNAME1","RSS News");
-define("_MI_TADRSS_BDESC1","List RSS News");
-define("_MI_TADRSS_SHOW_NUM","News number");
-define("_MI_TADRSS_SHOW_NUM_DESC","Set the number for each site news");
-?>
+define('_MI_TADRSS_NAME', 'Tad RSS News');
+define('_MI_TADRSS_AUTHOR', 'Tad (tad0616@gmail.com)');
+define('_MI_TADRSS_CREDITS', 'Tad');
+define('_MI_TADRSS_DESC', 'Adding RSS/Atom syndicated content inside your XOOPS Site.');
+define('_MI_TADRSS_ADMENU1', 'Admin');
+define('_MI_TADRSS_BNAME1', 'RSS News');
+define('_MI_TADRSS_BDESC1', 'List RSS News');
+define('_MI_TADRSS_SHOW_NUM', 'News number');
+define('_MI_TADRSS_SHOW_NUM_DESC', 'Set the number for each site news');
+
+define('_MI_TADRSS_DIRNAME', basename(dirname(dirname(__DIR__))));
+define('_MI_TADRSS_HELP_HEADER', __DIR__ . '/help/helpheader.html');
+define('_MI_TADRSS_BACK_2_ADMIN', 'Back to Administration of ');
+
+//help
+define('_MI_TADRSS_HELP_OVERVIEW', 'Overview');
