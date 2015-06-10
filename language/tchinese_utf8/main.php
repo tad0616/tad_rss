f8aa863b9da52ffdd75d53c1afea96a15ae2afab
Tad <tad0616@gmail.com>
2015-06-10 14:34:10 +0800

    調整語系

--- language/tchinese_utf8/main.php
+++ language/tchinese_utf8/main.php
@@ -5,5 +5,4 @@
 // $Id:$
 // ------------------------------------------------------------------------- //
 
-define("_TAD_NEED_TADTOOLS"," 需要 modules/tadtools，可至<a href='http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50' target='_blank'>Tad教材網</a>下載。");
-?>
+define('_TAD_NEED_TADTOOLS', ' 需要 modules/tadtools，可至<a href="http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50" target="_blank">Tad教材網</a>下載。');
--- language/tchinese_utf8/modinfo.php
+++ language/tchinese_utf8/modinfo.php
@@ -4,15 +4,14 @@
 // 製作日期：2009-10-20
 // $Id:$
 // ------------------------------------------------------------------------- //
-include_once XOOPS_ROOT_PATH."/modules/tadtools/language/{$xoopsConfig['language']}/modinfo_common.php";
+include_once XOOPS_ROOT_PATH . '/modules/tadtools/language/' . $xoopsConfig['language'] . '/modinfo_common.php';
 
-define("_MI_TADRSS_NAME","友站消息");
-define("_MI_TADRSS_AUTHOR","Tad (tad0616@gmail.com)");
-define("_MI_TADRSS_CREDITS","tad");
-define("_MI_TADRSS_DESC","此模組用來讀取他站的RSS新聞");
+define("_MI_TADRSS_NAME", "友站消息");
+define("_MI_TADRSS_AUTHOR", "Tad (tad0616@gmail.com)");
+define("_MI_TADRSS_CREDITS", "tad");
+define("_MI_TADRSS_DESC", "此模組用來讀取他站的RSS新聞");
 define("_MI_TADRSS_ADMENU1", "主管理介面");
-define("_MI_TADRSS_BNAME1","友站消息");
-define("_MI_TADRSS_BDESC1","友站消息(tad_rss_show)");
-define("_MI_TADRSS_SHOW_NUM","每個網站秀出新聞數");
-define("_MI_TADRSS_SHOW_NUM_DESC","設定每個網站新聞出現的數量");
-?>
+define("_MI_TADRSS_BNAME1", "友站消息");
+define("_MI_TADRSS_BDESC1", "友站消息(tad_rss_show)");
+define("_MI_TADRSS_SHOW_NUM", "每個網站秀出新聞數");
+define("_MI_TADRSS_SHOW_NUM_DESC", "設定每個網站新聞出現的數量");
