a3c0638282c5d229213ebfe9cddf0a6a0c7e0184
mambax7 <mambax7@gmail.com>
2019-04-10 11:02:51 -0400

    utility
    Signed-off-by: mambax7 <mambax7@gmail.com>

--- include/onUpdate.php
+++ include/onUpdate.php
@@ -1,136 +1,19 @@
 <?php
+
+use XoopsModules\Tad_rss\Utility;
+
 function xoops_module_update_tad_rss(&$module, $old_version)
 {
     global $xoopsDB;
 
-    //if(!chk_chk1()) go_update1();
-    chk_tad_rss_block();
+    //if(!tad_rss_chk_chk1()) tad_rss_go_update1();
+    Utility::chk_tad_rss_block();
 
-    mk_dir(XOOPS_ROOT_PATH . '/uploads/tad_rss');
-    mk_dir(XOOPS_ROOT_PATH . '/uploads/tad_rss/thumbs');
+    Utility::mk_dir(XOOPS_ROOT_PATH . '/uploads/tad_rss');
+    Utility::mk_dir(XOOPS_ROOT_PATH . '/uploads/tad_rss/thumbs');
+
     return true;
 }
 
-//刪除錯誤的重複欄位及樣板檔
-function chk_tad_rss_block()
-{
-    global $xoopsDB;
-    //die(var_export($xoopsConfig));
-    include XOOPS_ROOT_PATH . '/modules/tad_rss/xoops_version.php';
-
-    //先找出該有的區塊以及對應樣板
-    foreach ($modversion['blocks'] as $i => $block) {
-        $show_func                = $block['show_func'];
-        $tpl_file_arr[$show_func] = $block['template'];
-        $tpl_desc_arr[$show_func] = $block['description'];
-    }
-
-    //找出目前所有的樣板檔
-    $sql = "SELECT bid,name,visible,show_func,template FROM `" . $xoopsDB->prefix("newblocks") . "`
-    WHERE `dirname` = 'tad_rss' ORDER BY `func_num`";
-    $result = $xoopsDB->query($sql);
-    while (list($bid, $name, $visible, $show_func, $template) = $xoopsDB->fetchRow($result)) {
-        //假如現有的區塊和樣板對不上就刪掉
-        if ($template != $tpl_file_arr[$show_func]) {
-            $sql = "delete from " . $xoopsDB->prefix("newblocks") . " where bid='{$bid}'";
-            $xoopsDB->queryF($sql);
-
-            //連同樣板以及樣板實體檔案也要刪掉
-            $sql = "delete from " . $xoopsDB->prefix("tplfile") . " as a
-            left join " . $xoopsDB->prefix("tplsource") . "  as b on a.tpl_id=b.tpl_id
-            where a.tpl_refid='$bid' and a.tpl_module='tad_rss' and a.tpl_type='block'";
-            $xoopsDB->queryF($sql);
-        } else {
-            $sql = "update " . $xoopsDB->prefix("tplfile") . "
-            set tpl_file='{$template}' , tpl_desc='{$tpl_desc_arr[$show_func]}'
-            where tpl_refid='{$bid}'";
-            $xoopsDB->queryF($sql);
-        }
-    }
-}
 
 
-
-//建立目錄
-if (!function_exists('mk_dir')) {
-    function mk_dir($dir = "")
-    {
-        //若無目錄名稱秀出警告訊息
-        if (empty($dir)) {
-            return;
-        }
-
-        //若目錄不存在的話建立目錄
-        if (!is_dir($dir)) {
-            umask(000);
-            //若建立失敗秀出警告訊息
-            mkdir($dir, 0777);
-        }
-    }
-}
-
-//拷貝目錄
-if (!function_exists('full_copy')) {
-    function full_copy($source = "", $target = "")
-    {
-        if (is_dir($source)) {
-            @mkdir($target);
-            $d = dir($source);
-            while (false !== ($entry = $d->read())) {
-                if ($entry == '.' || $entry == '..') {
-                    continue;
-                }
-
-                $Entry = $source . '/' . $entry;
-                if (is_dir($Entry)) {
-                    full_copy($Entry, $target . '/' . $entry);
-                    continue;
-                }
-                copy($Entry, $target . '/' . $entry);
-            }
-            $d->close();
-        } else {
-            copy($source, $target);
-        }
-    }
-}
-
-if (!function_exists('rename_win')) {
-    function rename_win($oldfile, $newfile)
-    {
-        if (!rename($oldfile, $newfile)) {
-            if (copy($oldfile, $newfile)) {
-                unlink($oldfile);
-                return true;
-            }
-            return false;
-        }
-        return true;
-    }
-}
-
-if (!function_exists('delete_directory')) {
-    function delete_directory($dirname)
-    {
-        if (is_dir($dirname)) {
-            $dir_handle = opendir($dirname);
-        }
-
-        if (!$dir_handle) {
-            return false;
-        }
-
-        while ($file = readdir($dir_handle)) {
-            if ($file != "." && $file != "..") {
-                if (!is_dir($dirname . "/" . $file)) {
-                    unlink($dirname . "/" . $file);
-                } else {
-                    delete_directory($dirname . '/' . $file);
-                }
-            }
-        }
-        closedir($dir_handle);
-        rmdir($dirname);
-        return true;
-    }
-}
