3e9c6eff99b284ca3e2ede40edce27f1a0ac42bc
mambax7 <mambax7@gmail.com>
2019-04-13 03:48:34 -0400

    cosmetics
    Signed-off-by: mambax7 <mambax7@gmail.com>

--- blocks/tad_rss_show.php
+++ blocks/tad_rss_show.php
@@ -1,20 +1,20 @@
 <?php
-include_once XOOPS_ROOT_PATH . "/modules/tad_rss/function_block.php";
+include_once XOOPS_ROOT_PATH . '/modules/tad_rss/function_block.php';
 
 //區塊主函式 (友站消息(tad_rss_show))
-function tad_rss_show($options = ["", 3, 170])
+function tad_rss_show($options = ['', 3, 170])
 {
     global $xoopsDB, $xoTheme;
     $xoTheme->addStylesheet('modules/tadtools/css/vertical_menu.css');
 
-    $in = (empty($options[0])) ? "" : "and rss_sn in({$options[0]})";
+    $in = (empty($options[0])) ? '' : "and rss_sn in({$options[0]})";
 
-    $modhandler        = xoops_getHandler('module');
-    $xoopsModule       = $modhandler->getByDirname("tad_rss");
-    $config_handler    = xoops_getHandler('config');
+    $modhandler = xoops_getHandler('module');
+    $xoopsModule = $modhandler->getByDirname('tad_rss');
+    $config_handler = xoops_getHandler('config');
     $xoopsModuleConfig = $config_handler->getConfigsByCat(0, $xoopsModule->getVar('mid'));
 
-    $sql = "select * from " . $xoopsDB->prefix("tad_rss") . " where enable='1' $in";
+    $sql = 'select * from ' . $xoopsDB->prefix('tad_rss') . " where enable='1' $in";
 
     $result = $xoopsDB->query($sql) or redirect_header($_SERVER['PHP_SELF'], 3, die($sql));
 
@@ -29,31 +29,31 @@ function tad_rss_show($options = ["", 3, 170])
 
         $rss = get_rss_by_simplepie_block($url, $options[1]);
 
-        $block['rss_data'][$n]['link']    = $url;
-        $block['rss_data'][$n]['title']   = $title;
+        $block['rss_data'][$n]['link'] = $url;
+        $block['rss_data'][$n]['title'] = $title;
         $block['rss_data'][$n]['content'] = $rss;
         $n++;
     }
+
     return $block;
 }
 
-//
 function tad_rss_show_edit($options)
 {
     global $xoopsDB;
 
     if (!empty($options[0])) {
-        $sc = explode(",", $options[0]);
+        $sc = explode(',', $options[0]);
     }
 
-    $js = "<script>
+    $js = '<script>
     function bbv(col){
-      \$i=0;
-      var arr = new Array();";
+      $i=0;
+      var arr = new Array();';
 
-    $chkbox = "";
+    $chkbox = '';
 
-    $sql = "SELECT * FROM " . $xoopsDB->prefix("tad_rss") . " WHERE enable='1'";
+    $sql = 'SELECT * FROM ' . $xoopsDB->prefix('tad_rss') . " WHERE enable='1'";
 
     $result = $xoopsDB->query($sql);
     while ($all = $xoopsDB->fetchArray($result)) {
@@ -67,7 +67,7 @@ function tad_rss_show_edit($options)
                 \$i++;
             }";
 
-        $chked = (in_array($rss_sn, $sc)) ? "checked" : "";
+        $chked = (in_array($rss_sn, $sc, true)) ? 'checked' : '';
         $chkbox .= "<input type='checkbox' id='c{$rss_sn}' value='{$rss_sn}'  onChange=bbv() $chked>$title";
     }
     $js .= "document.getElementById('bb').value=arr.join(',');
@@ -90,30 +90,31 @@ function tad_rss_show_edit($options)
             </div>
         </li>
     </ol>";
+
     return $form;
 }
 
 //以 simplepie 來取得RSS
-function get_rss_by_simplepie_block($url = "", $maxitems = 5)
+function get_rss_by_simplepie_block($url = '', $maxitems = 5)
 {
     require_once XOOPS_ROOT_PATH . '/modules/tad_rss/class/simplepie/SimplePie.php';
     $feed = new SimplePie();
     $feed->set_output_encoding(_CHARSET);
     $feed->set_feed_url($url);
-    $feed->set_cache_location(XOOPS_ROOT_PATH . "/uploads/simplepie_cache");
+    $feed->set_cache_location(XOOPS_ROOT_PATH . '/uploads/simplepie_cache');
     $feed->init();
     $feed->handle_content_type();
 
     $n = 0;
     foreach ($feed->get_items(0, $maxitems) as $item) {
-        $href        = $item->get_permalink();
-        $title       = $item->get_title();
-        $date        = $item->get_date("m/d");
+        $href = $item->get_permalink();
+        $title = $item->get_title();
+        $date = $item->get_date('m/d');
         $description = $item->get_description();
 
-        $arr[$n]['date']        = $date;
-        $arr[$n]['link']        = $href;
-        $arr[$n]['title']       = $title;
+        $arr[$n]['date'] = $date;
+        $arr[$n]['link'] = $href;
+        $arr[$n]['title'] = $title;
         $arr[$n]['description'] = $description;
         $n++;
     }
