8180ca83492a365e208658ccddca9e0bc4f3a181
tad0616 <tad0616@gmail.com>
2019-05-10 22:20:04 +0800

    修改說明

--- include/onUpdate.php
+++ include/onUpdate.php
@@ -2,6 +2,12 @@
 
 use XoopsModules\Tadtools\Utility;
 use XoopsModules\Tad_rss\Update;
+if (!class_exists('XoopsModules\Tadtools\Utility')) {
+    require XOOPS_ROOT_PATH . '/modules/tadtools/preloads/autoloader.php';
+}
+if (!class_exists('XoopsModules\Tad_rss\Update')) {
+    include dirname(__DIR__) . '/preloads/autoloader.php';
+}
 
 function xoops_module_update_tad_rss(&$module, $old_version)
 {
