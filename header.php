5526ea7170adc8fad75cbb42a5fea2063b3c057d
tad0616 <tad0616@gmail.com>
2024-10-30 15:31:28 +0800

    新增interface.php

--- header.php
+++ header.php
@@ -1,13 +1,3 @@
 <?php
-
-use XoopsModules\Tadtools\Utility;
-
 require_once dirname(dirname(__DIR__)) . '/mainfile.php';
-
-//判斷是否對該模組有管理權限
-if (!isset($_SESSION['tad_rss_adm'])) {
-    $_SESSION['tad_rss_adm'] = ($xoopsUser) ? $xoopsUser->isAdmin() : false;
-}
-
-$interface_menu[_MD_TADRSS_INDEX] = 'index.php';
-$interface_icon[_MD_TADRSS_INDEX] = "fa-rss";
+require_once __DIR__ . '/interface.php';
